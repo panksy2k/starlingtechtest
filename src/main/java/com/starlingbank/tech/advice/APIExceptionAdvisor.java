cbaab55a2ebac7e4630676708a9fb23a0ab11184
Pankaj Pardasani <pankaj.d.p@me.com>
2019-12-28 18:48:52 +0000

    added handler in REST advice for business exception

--- src/main/java/com/starlingbank/tech/advice/APIExceptionAdvisor.java
+++ src/main/java/com/starlingbank/tech/advice/APIExceptionAdvisor.java
@@ -1,6 +1,7 @@
 package com.starlingbank.tech.advice;
 
 import com.starlingbank.tech.domain.ErrorResponse;
+import com.starlingbank.tech.exception.StarlingBusinessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,5 +18,10 @@ public class APIExceptionAdvisor extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { IllegalArgumentException.class, IllegalStateException.class, ConstraintViolationException.class })
     protected ResponseEntity<Object> handleConflictConstraints(RuntimeException ex, WebRequest request) {
         return new ResponseEntity(new ErrorResponse(1, ex.getMessage()), new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(value = { StarlingBusinessException.class })
+    protected ResponseEntity<Object> handleBusinessOrTechnicalError(StarlingBusinessException ex, WebRequest request) {
+        return new ResponseEntity(new ErrorResponse(500, ex.getMessage()), new HttpHeaders(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
